029a726abb3581b1c448bc7d5b1a12a15b8d7f09
David Strauß <mail@stravid.com>
2013-04-11 09:25:37 +0200

    Another try

--- app/assets/javascripts/active_admin/editor/parser_rules.js
+++ app/assets/javascripts/active_admin/editor/parser_rules.js
@@ -90,7 +90,7 @@
        *                          - clear_br:    converts clear attribute values left/right/all/both to their corresponding css class "wysiwyg-clear-*"
        *                            <br clear="all"> ... becomes ... <br class="wysiwyg-clear-both">
        *                          - align_img:    converts align attribute values (right/left) on <img> to their corresponding css class "wysiwyg-float-*"
-       *                          
+       *
        *    - remove:             removes the element and its content
        *
        *    - rename_tag:         renames the element to the given tag
@@ -185,7 +185,7 @@
           },
           "a": {
               "check_attributes": {
-                  "href": "url" // if you compiled master manually then change this from 'url' to 'href'
+                  "href": "href" // if you compiled master manually then change this from 'url' to 'href'
               },
               "set_attributes": {
                   "target": "_blank"
